64911be3df9edb980714fc5e60e3d5985ef9e2cf
Nora668 <48057759+Nora668@users.noreply.github.com>
2019-07-16 15:50:28 -0400

    Add files via upload

---
+++ web.js
@@ -0,0 +1,10 @@
+alert("Welcome to my page!");
+
+function changecolor(color, id){
+  element.style.color = color;
+}
+
+function showcontent(id){
+  var element = document.getElementById(id);
+  element.style.display = 'block';
+}
